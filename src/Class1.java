5edcff129f34c076adb559ee83447257d47a3137
Todd Booth <Git1@ToddBooth.Com>
2022-05-15 23:24:35 +0200

    commit_2

--- src/Class1.java
+++ src/Class1.java
@@ -1,8 +1,11 @@
+import org.testng.annotations.Test;
+
 public class Class1 {
-    public void Class1() {
+    public Class1() {
         System.out.println("Hello Class1.");
     }
 
+    @Test
     public void method_1() {
         System.out.println("Hello method_1");
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import org.testng.annotations.Test;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World.");
     }
 
+    @Test
     public static void method_1() {
         System.out.println("Hello method_1");
     }
