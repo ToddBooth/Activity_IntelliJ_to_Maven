ccd64063c03c9f1507bdd950cf1328e59ca26842
Todd Booth <Git1@ToddBooth.Com>
2022-05-15 22:35:09 +0200

    commit_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
+import org.testng.annotations.Test;
+
+class Main {
     public static void main(String[] args) {
         System.out.println("Hello World.");
+    }
+
+    public static void main() {
+        System.out.println("Hello World 2.");
+    }
+
+    public static void method_1() {
+        System.out.println("Hello method_1");
     }
 }
